212bdbccb669fe0dac6bdfb4d9af2beca7d2cb94
magmafellow <magmafellow@gmail.com>
2024-07-23 22:08:57 +0300

    vers 1.0.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Tomato from './ui/page'
+import Tomato from './ui/tomato'
 import { useEffect, useState } from 'react'
 import clsx from 'clsx'
 
@@ -71,7 +71,10 @@ export default function Page() {
       }
     }, 1000)
 
-    return () => {clearInterval(intervalId); clearInterval(intervalIdRelax)}
+    return () => {
+      clearInterval(intervalId)
+      clearInterval(intervalIdRelax)
+    }
   })
 
   const tomatoesArrayPlaceholder = Array.from(
@@ -81,36 +84,57 @@ export default function Page() {
 
   const goHandler = function () {
     if (status === 'relax waiting') {
+      if (getTotalSecsFromTimeObj(getSelectValuesRelax()) === 0) return
       setStatus('relaxing...')
       setChosenTimeRelax(getTotalSecsFromTimeObj(getSelectValuesRelax()))
-
-    } else if (status !== 'relax waiting') {
+    } else if (status === 'pausing relax...') {
+      setStatus('relaxing...')
+    } else if (status === 'non-active' || status === 'relaxed') {
+      if (getTotalSecsFromTimeObj(getSelectValues()) === 0) return
       setStatus('going...')
       setChosenTime(getTotalSecsFromTimeObj(getSelectValues()))
+    } else if (status === 'pausing...') {
+      setStatus('going...')
     }
+    // else if (
+    //   status === 'non-active' ||
+    //   status === 'relaxed' ||
+    //   status === 'pausing...'
+    // ) {
+    //   if (getTotalSecsFromTimeObj(getSelectValues()) === 0) return
+    //   setStatus('going...')
+    //   setChosenTime(getTotalSecsFromTimeObj(getSelectValues()))
+    // }
   }
   const takePause = function () {
-    setStatus('pausing...')
+    if (status === 'relaxing...') {
+      setStatus('pausing relax...')
+    } else if (status === 'going...') {
+      setStatus('pausing...')
+    }
   }
   const resetHandler = function () {
-    setStatus('non-active')
-    setSecs(0)
-    setSecsRelax(0)
-    setChosenTime(0)
-    setChosenTimeRelax(0)
-    
+    if (status === 'relaxing...' || status === 'pausing relax...') {
+      setStatus('relax waiting')
+      setSecsRelax(0)
+      setChosenTimeRelax(0)
+    } else if (status === 'going...' || status === 'pausing...') {
+      setStatus('non-active')
+      setSecs(0)
+      setChosenTime(0)
+    }
   }
 
   return (
-    <div>
-      <h1 className="font-semibold text-[50px] mb-10 text-red-500 text-center">
+    <div className="px-2">
+      <h1 className="font-semibold text-[25px] sm:text-[35px] lg:text-[50px] pt-2 mb-4 sm:mb-6 lg:mb-10 text-red-500 text-center">
         Tomato focus
       </h1>
       <div className="grid">
         {/* tomatoes */}
         <div className="flex min-h-[102px] flex-wrap mb-10 gap-10 border-red-200 border-2 pt-5 pb-2 px-4 rounded-md">
-          {tomatoesArrayPlaceholder.map(() => (
-            <Tomato />
+          {tomatoesArrayPlaceholder.map((item, index) => (
+            <Tomato key={index} />
           ))}
           {tomatoesDone === 0 && (
             <span className="text-[24px] sm:text-[32px] md:text-[44px] lg:text-[56px]">
@@ -120,24 +144,59 @@ export default function Page() {
         </div>
         {/* timer or relax */}
         <div
-          className={clsx('text-[100px] font-semibold text-center', {
-            'text-neutral-600': status === 'relax waiting' || status === 'relaxing...',
-            'text-white': status !== 'relax waiting' && status !== 'relaxing...',
-          })}
+          className={clsx(
+            'text-[50px] sm:text-[70px] md:text-[80px] lg:text-[100px] mb-4 sm:mb-7 font-semibold text-center',
+            {
+              'text-neutral-600':
+                status === 'relax waiting' ||
+                status === 'relaxing...' ||
+                status === 'pausing relax...',
+              'text-white':
+                status !== 'relax waiting' &&
+                status !== 'relaxing...' &&
+                status !== 'pausing relax...',
+            }
+          )}
         >
           {showTimeFromSeconds(secs)}
         </div>
-        {(status === 'relax waiting' || status === 'relaxing...') && (
-          <div className="text-[35px] font-semibold text-center">
+        {(status === 'relax waiting' ||
+          status === 'relaxing...' ||
+          status === 'pausing relax...') && (
+          <div className="text-[30px] sm:text-[40px] md:text-[42px] lg:text-[45px] font-semibold text-center">
             {showTimeFromSeconds(secsRelax)}
           </div>
         )}
 
         {/* <Timer secs={secs} /> */}
+
+        {/* chosen time */}
+        <div>
+          {(status === 'pausing...' || status === 'going...') && (
+            <p className="text-xl tracking-wider">
+              Tomato time{' '}
+              <span className="text-2xl bg-neutral-700 rounded border border-neutral-200 py-0.5 px-1">
+                {showTimeFromSeconds(chosenTime)}
+              </span>
+            </p>
+          )}
+          {(status === 'relaxing...' || status === 'pausing relax...') && (
+            <p className="text-xl">
+              Relax time{' '}
+              <span className="text-2xl bg-neutral-700 rounded border border-neutral-200 py-0.5 px-1">
+                {showTimeFromSeconds(chosenTimeRelax)}
+              </span>
+            </p>
+          )}
+        </div>
+
         {/* input */}
         <div>
           {/* container for tomato time */}
-          {status !== 'relax waiting' && status !== 'relaxing...' && (
+          {/* (status !== 'relax waiting' &&
+            status !== 'relaxing...' &&
+            status !== 'pausing relax...') */}
+          {(status === 'non-active' || status === 'relaxed') && (
             <div>
               <label className="text-lg mr-1" htmlFor="mins">
                 minutes
@@ -147,8 +206,6 @@ export default function Page() {
                 name="mins"
                 id="mins"
               >
-              
-                <option value="00">00</option>
                 <option value="20">20</option>
                 <option selected value="25">
                   25
@@ -163,7 +220,7 @@ export default function Page() {
                 name="secs"
                 id="secs"
               >
-                <option value="5">5</option>
+                <option value="5">05</option>
                 <option value="20">20</option>
                 <option value="30">30</option>
                 <option value="40">40</option>
@@ -176,7 +233,10 @@ export default function Page() {
           )}
 
           {/* container for relax time */}
-          {(status === 'relax waiting' || status === 'relaxing...') && (
+          {/* (status === 'relax waiting' ||
+            status === 'relaxing...' ||
+            status === 'pausing relax...') */}
+          {status === 'relax waiting' && (
             <div>
               <label className="text-lg mr-1" htmlFor="mins">
                 minutes
@@ -186,12 +246,24 @@ export default function Page() {
                 name="mins_relax"
                 id="mins_relax"
               >
-                <option value="00">00</option>
-                <option selected value="05">
-                  05
-                </option>
-                <option value="05">05</option>
-                <option value="15">15</option>
+                {tomatoesDone !== 0 && tomatoesDone % 4 === 0 ? (
+                  <>
+                    <option value="20">20</option>
+                    <option selected value="15">
+                      15
+                    </option>
+                    <option value="12">12</option>
+                    <option value="10">10</option>
+                  </>
+                ) : (
+                  <>
+                    <option selected value="5">
+                      05
+                    </option>
+                    <option value="6">06</option>
+                    <option value="7">07</option>
+                  </>
+                )}
               </select>
               <label className="text-lg mr-1" htmlFor="secs">
                 seconds
@@ -201,7 +273,7 @@ export default function Page() {
                 name="secs_relax"
                 id="secs_relax"
               >
-                <option value="5">5</option>
+                <option value="5">05</option>
                 <option value="20">20</option>
                 <option value="30">30</option>
                 <option value="40">40</option>
@@ -224,7 +296,9 @@ export default function Page() {
               className="py-2 px-4 rounded bg-amber-500 font-semibold"
               onClick={goHandler}
             >
-              {status === 'relax waiting' ? 'relax' : 'GO!'}
+              {status === 'relax waiting' || status === 'pausing relax...'
+                ? 'relax'
+                : 'GO!'}
             </button>
             <button
               className="py-2 px-4 rounded bg-red-800 font-semibold"
--- app/ui/tomato.tsx
+++ app/ui/tomato.tsx
@@ -1,7 +1,7 @@
 
 export default function Tomato({ saturate, scale }: { saturate?: string, scale?: string }) {
   return (
-      <div className={`${saturate} ${scale} w-[100px] h-[70px] bg-red-600 rounded-[36px] relative border-4 border-red-950 outline outline-1 outline-red-300 transition-all`}>
+      <div className={`${saturate} ${scale} scale-75 sm:scale-[.95] md:scale-[1.05] lg:scale-[1.1] w-[100px] h-[70px] bg-red-600 rounded-[36px] relative border-4 border-red-950 outline outline-1 outline-red-300 transition-all`}>
         {/* leaves */}
         <div className="h-[45px] w-[10px] bg-green-500 absolute left-7 -top-6 -rotate-45"></div>
         <div className="h-[40px] w-[8px] bg-green-500 absolute left-16 -top-5 rotate-45"></div>
